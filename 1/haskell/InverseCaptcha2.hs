9b0aff6c51e92974c638bfe59efa4453a716a077
tomcs <code@getbraintree.com>
2017-12-01 17:10:32 -0800

    day 1: second part in haskell

---
+++ 1/haskell/InverseCaptcha2.hs
@@ -0,0 +1,41 @@
+module InverseCaptcha2 where
+
+import Data.Char
+import Data.Either
+import Text.Printf
+import qualified Data.Text as T
+
+strip  = T.unpack . T.strip . T.pack
+
+testCases = [
+              ("1212", 6),
+              ("1221", 0),
+              ("123425", 4),
+              ("123123", 12),
+              ("12131415", 4)
+            ]
+
+transformInput :: String -> [Int]
+transformInput = (map digitToInt) . strip
+
+check :: Int -> Int -> Int
+check a b = if a == b
+            then a + b
+            else 0
+
+calculateSum :: [Int] -> Int
+calculateSum xs = foldr (+) 0 $ zipWith (check) (take mid xs) (drop mid xs)
+                  where mid = (length xs) `div` 2
+
+test :: [(String, Int)] -> [Either String String]
+test = map checkCase
+       where checkCase (input, expect) = if ((calculateSum . transformInput) input == expect)
+             then Right $ printf "Test case %s passed!" (show input)
+             else Left $ printf "Expected %s for %s, got %d." (show expect) (show input) ((calculateSum . transformInput) input)
+
+main :: IO ()
+main = do
+      if length (lefts (test testCases)) > 0
+      then print $ lefts (test testCases)
+      else do input <- readFile "../input"
+              print $ (calculateSum . transformInput) input
