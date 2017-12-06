ff9a97037b33b2ceddec4828ab8dd8be94cd42ea
Tom Christiansen-Salameh <tcsalameh@uchicago.edu>
2017-12-05 22:54:49 -0800

    reimplement day 6 with vectors instead of maps

--- 6/haskell/RepeatingBanks.hs
+++ 6/haskell/RepeatingBanks.hs
@@ -3,7 +3,7 @@ module RepeatingBanks2 where
 import Data.List.Split (splitOn)
 import Text.Printf
 import Data.Either (isLeft)
-import qualified Data.Map as M
+import qualified Data.Vector as V
 import qualified Data.Set as S
 
 input = "14\t0\t15\t12\t11\t11\t3\t5\t1\t6\t8\t4\t9\t1\t8\t4"
@@ -17,36 +17,34 @@ maxBlock (k1, v1) (k2, v2)
     | v1 == v2  = if k1 < k2 then (k1, v1) else (k2, v2)
     | otherwise = (k1, v1)
 
-findHighestBlock :: M.Map Int Int -> (Int,Int)
-findHighestBlock = M.foldrWithKey (\k v acc -> maxBlock (k,v) acc) (maxBound::Int,-1)
+findHighestBlock :: V.Vector Int -> (Int,Int)
+findHighestBlock = V.ifoldr (\i v acc -> maxBlock (i,v) acc) (maxBound::Int,-1)
 
-zero :: M.Map Int Int -> Int -> M.Map Int Int
-zero banks bankNum = M.insert bankNum 0 banks
+zero :: Int -> V.Vector Int -> V.Vector Int
+zero bankNum v = v V.// [(bankNum, 0)]
 
-redistribute :: M.Map Int Int -> M.Map Int Int
-redistribute banks = go (zero banks (fst highestBlock)) highestBlock
-    where go m (_,0) = m
-          go m (k,v) = go (inc (next k) m) (next k,v-1)
-          next i = if i < (bankSize-1) then (i+1) else 0
-          inc idx m = M.insert idx ((m M.! idx) + 1) m
-          bankSize = M.size banks
+redistribute :: V.Vector Int -> V.Vector Int
+redistribute banks = go (zero (fst highestBlock) banks) highestBlock
+    where go v (_,0) = v
+          go v (i,x) = go (inc (next i) v) (next i,x-1)
+          next i = if i < (banksize-1) then (i+1) else 0
+          inc i v = v V.// [(i, (v V.! i) + 1)]
+          banksize = length banks
           highestBlock = findHighestBlock banks
 
-countUntilDup :: M.Map Int Int -> (M.Map Int Int, Int)
-countUntilDup banks = counter banks set 0
-        where counter m s n = if S.member m s
-                              then (m, n)
-                              else counter (next m) (S.insert m s) (n+1)
-              set = S.empty
-              next m = redistribute m
+countUntilDup :: V.Vector Int -> (V.Vector Int, Int)
+countUntilDup banks = counter banks S.empty 0
+        where counter v s n = if S.member v s
+                              then (v, n)
+                              else counter (redistribute v) (S.insert v s) (n+1)
 
-test :: M.Map Int Int -> Int -> Either String String
+test :: V.Vector Int -> Int -> Either String String
 test input exp = if (snd . countUntilDup) input == exp
                     then Right "Test case passed!"
                     else Left $ printf "Expected %s for %s, got %s" (show exp) (show input) (show $ countUntilDup input)
 
-parseInput :: String -> M.Map Int Int
-parseInput s = M.fromList $ zip [0..] $ map (\x -> read x::Int) $ splitOn "\t" s
+parseInput :: String -> V.Vector Int
+parseInput s = V.fromList $ map (\x -> read x::Int) $ splitOn "\t" s
 
 main :: IO ()
 main = do
